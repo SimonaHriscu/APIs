7235d3eb7d24734c0c56735f814c6c6c308a4edc
Simona <see.moona@yahoo.com>
2020-06-10 13:48:44 +0200

    dictionary app simple v.

--- Dictionary App/src/index.js
+++ Dictionary App/src/index.js
@@ -5,23 +5,26 @@ const word_id = process.argv[2];
 const language = "en-gb"
 const url = "https://od-api.oxforddictionaries.com:443/api/v2/entries/" + language + "/" + word_id.toLowerCase();
 
+
 axios.get(url, {
-    method: 'GET',
-    headers: { //HERE IS THE DIFFERENCE
+    headers: { //HERE IS THE KEY
       'app_id': myKey.app_id, 'app_key': myKey.app_key
-    },
-    mode: 'no-cors'
+    }
    })
+
     .then(res => res.data) 
     //handles response
 
-   .then(data => 
-   
-    console.log(`    ${data.id}(${data.results[0].lexicalEntries[0].lexicalCategory.text})
-          1. ${data.results[0].lexicalEntries[0].entries[0].senses[0].definitions[0]}
-          2. ${data.results[0].lexicalEntries[0].entries[0].senses[1].definitions[0]}
-          3. ${data.results[0].lexicalEntries[0].entries[0].senses[2].definitions[0]}
-    Provided by: ${data.metadata.provider} `))
+   .then(data => {
+     const {results} = data;
+     const arrData = results[0].lexicalEntries[0].entries[0].senses;
+     console.log(`  ${data.id}(${data.results[0].lexicalEntries[0].lexicalCategory.text})
+      `)
+     arrData.forEach((sense, index) => {
+       console.log(`    ${(index + 1).toString()} ${sense.shortDefinitions.join(' ')};`);
+     })
+     console.log(`Provided by: ${data.metadata.provider}. `)
+   })
     //handles exact data
     
     .catch(err => console.log(err));
